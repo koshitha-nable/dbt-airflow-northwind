28a7d366c1140f07c9a1ffcb2c0887b60ba2704e
Koshitha Amarakoon <koshithaa@n-able.biz>
2023-07-22 13:53:11 +0530

    Created notification function

--- dags/init.py
+++ dags/init.py
@@ -16,11 +16,55 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+def handle_failure(context):
+    # This function will be called whenever a task fails in the DAG
+    failed_task = context.get('task_instance')
+    failed_task_id = failed_task.task_id
+
+    # Perform actions or create additional tasks specific to handling failure scenarios
+    # For example, you can send a notification, trigger a recovery process, or perform cleanup tasks.
+
+    # Send a notification
+    send_notification()
+
+
+def send_notification():
+    # Email configuration
+    sender_email = 'savindukoshitha.a@gmail.com'
+    recipient_email = 'koshithaa@n-able.biz'
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_username = 'savindukoshitha.a@gmail.com'
+    smtp_password = ''
+
+    # Email content
+    subject = 'Airflow DAG Execution Failure'
+    body = 'An error occurred while executing the DAG. Please check the logs for more details.'
+
+    # Construct the email message
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = recipient_email
+    message['Subject'] = subject
+    message.attach(MIMEText(body, 'plain'))
+
+    # Send the email
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_username, smtp_password)
+            server.send_message(message)
+        print('Email notification sent successfully!')
+    except Exception as e:
+        print(f'Failed to send email notification. Error: {str(e)}')
+
+
 dag = DAG(
     'dbt_airflow_sample',
     default_args=default_args,
     description='A simple dbt and Airflow init DAG',
     schedule_interval='@once',
+    on_failure_callback=handle_failure,  # Specify the failure handling function
 )
 
 
